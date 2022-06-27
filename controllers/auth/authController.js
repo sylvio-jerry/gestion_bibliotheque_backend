98ccd31d667a5e0d3ed8aa3c7d438c1f8f0e9758
Raphaël <raphael.cs.en@gmail.com>
2022-06-27 20:19:07 +0300

    updated authController

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -48,10 +48,10 @@ module.exports = {
     login: async(req, res, next) => {
         // Request validation 
         const { error } = validate(req.body)
-        if(error) sendError(res, error.details[0].message)
+        if(error) sendResponse(res, null, error.details[0].message)
 
         const { pseudo, password } = req.body
-        if(!pseudo || !password) sendError(res, "Fill the required fields")
+        if(!pseudo || !password) sendResponse(res, null, "Fill the required fields")
 
         // Check if the pseudo exists 
         const user = await users.findFirst({
@@ -59,7 +59,7 @@ module.exports = {
                 pseudo
             }
         })
-        if(!user) sendError(res, "User does not exist")
+        if(!user) sendResponse(res, null, "User does not exist")
 
         // Check if the password correct 
         bcrypt.compare(password, user.password, async (err, result) => {
@@ -83,7 +83,7 @@ module.exports = {
 
                 sendResponse(res, access_token ,"User logged in successfully")
             } else {
-                sendError(res, "Invalid password")
+                sendResponse(res, null,"Invalid password")
             }
         }) 
     }, 
@@ -96,8 +96,38 @@ module.exports = {
 
         // Clear cookie 
         res.clearCookie('auth_token', { httpOnly: true, sameSite: 'None', secure: true })
+        
         // Send response 
         sendResponse(res, null, "User logged out successfully")
+    }, 
+
+    update: async (req, res, next) => {
+        // Validate request 
+        const { error } = validate(req.body)
+        if(error) sendError(res, error.details[0].message)
+
+        const { fullname, pseudo, password } = req.body
+        // Check fullname and pseudo 
+        const user = await users.findFirst({
+            where: { fullname, pseudo }
+        })
+
+        if(!user) sendResponse(res, null, "User does not exist")
+        
+        // Hash password 
+        const hash = await bcrypt.hash(password, 10)
+
+        // Update password 
+        const updatedUser = await users.update({
+            where: { id: +user.id }, 
+            data: {
+                password: hash
+            }
+        })
+        // exclude password from the response 
+        delete updatedUser.password 
+        // send response 
+        sendResponse(res, updatedUser, "Password updated successfully")
     }
 }
 
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,5 +4,6 @@ const user = require('../controllers/auth/authController')
 router.post('/register', user.register)
 router.post('/login', user.login)
 router.post('/logout', user.logout)
+router.put('/update', user.update)
 
 module.exports = router
